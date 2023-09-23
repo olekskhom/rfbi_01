9351eea7d13e40c7e5059cfb28ffa1feb704eaa7
olekskhom <oleks.khom@gmail.com>
2023-09-23 21:34:08 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>test</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione tenetur quod voluptates eos expedita, deleniti vero aliquam  pariatur quisquam, culpa quam? Reprehenderit provident molestias veritatis minus! Quos!</p>
     </div>
   );
 }
